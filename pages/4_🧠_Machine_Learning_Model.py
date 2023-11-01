b390b53bf3d5ffae8851c0fd1cec06b6156dc99b
Stephen Chacha <chacharawe.stephen@gmail.com>
2023-11-01 16:09:00 +0300

    Update 4_🧠_Machine_Learning_Model.py

--- pages/4_🧠_Machine_Learning_Model.py
+++ pages/4_🧠_Machine_Learning_Model.py
@@ -61,19 +61,22 @@ st.write(f'The model Accuracy is : {accur}%')
 
 # get user input
 st.subheader('User Inputs')
-Age=st.number_input('Employee age')
-EmpDepartment = st.selectbox('Employee Department: Data Science-0  Development-1  Finance-2  Human Resources-3  Research & Development-4  Sales-5',('0','1','2','3','4','5'))
-EmpJobRole = st.selectbox('Employee Role:Business Analyst-0 Data Scientist-1 Delivery Manager-2  Developer-3 Finance Manager-4 Healthcare Representative-5 Human Resources-6 Laboratory Technician-7 Manager-8 Manager R&D-9 Manufacturing Director-10 Research Director-11 Research Scientist-12 Sales Executive-13 Sales Representative-14 Senior Developer-15 Senior Manager R&D-16  Technical Architect-17 Technical Lead-18',(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18))
-DistanceFromHome=st.number_input('Distance from home in miles')
-EmpEnvironmentSatisfaction = st.selectbox('Environment Satisfaction: 1-Low, 2-Medium, 3-High, 4-Very High',(1,2,3,4))
-EmpHourlyRate=st.number_input('Hourly Rate in Dollars')
-EmpLastSalaryHikePercent=st.number_input('Salary Hike Percentage')
-TotalWorkExperienceInYears=st.number_input('Work Experience')
-EmpWorkLifeBalance = st.selectbox('Work-Life Balance: 1-Low, 2-Medium, 3-High, 4-Very High',(1,2,3,4))
-ExperienceYearsAtThisCompany=st.number_input('Experience in this Company: Years')
-ExperienceYearsInCurrentRole=st.number_input('Experience in this Role: Years')
-YearsSinceLastPromotion= st.number_input('Time since last promotion: Years')
-YearsWithCurrManager=st.number_input('Years with current manager')
+col1,col2=st.columns()
+with col1:
+    Age=st.number_input('Employee age')
+    EmpDepartment = st.selectbox('Employee Department: Data Science-0  Development-1  Finance-2  Human Resources-3  Research & Development-4  Sales-5',('0','1','2','3','4','5'))
+    EmpJobRole = st.selectbox('Employee Role:Business Analyst-0 Data Scientist-1 Delivery Manager-2  Developer-3 Finance Manager-4 Healthcare Representative-5 Human Resources-6 Laboratory Technician-7 Manager-8 Manager R&D-9 Manufacturing Director-10 Research Director-11 Research Scientist-12 Sales Executive-13 Sales Representative-14 Senior Developer-15 Senior Manager R&D-16  Technical Architect-17 Technical Lead-18',(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18))
+    DistanceFromHome=st.number_input('Distance from home in miles')
+    EmpEnvironmentSatisfaction = st.selectbox('Environment Satisfaction: 1-Low, 2-Medium, 3-High, 4-Very High',(1,2,3,4))
+    EmpHourlyRate=st.number_input('Hourly Rate in Dollars')
+    EmpLastSalaryHikePercent=st.number_input('Salary Hike Percentage')
+with col2:
+    TotalWorkExperienceInYears=st.number_input('Work Experience')
+    EmpWorkLifeBalance = st.selectbox('Work-Life Balance: 1-Low, 2-Medium, 3-High, 4-Very High',(1,2,3,4))
+    ExperienceYearsAtThisCompany=st.number_input('Experience in this Company: Years')
+    ExperienceYearsInCurrentRole=st.number_input('Experience in this Role: Years')
+    YearsSinceLastPromotion= st.number_input('Time since last promotion: Years')
+    YearsWithCurrManager=st.number_input('Years with current manager')
 
 # create a new dataframe to display selected input
 pred1 = pd.DataFrame({'Age':Age,'EmpDepartment':EmpDepartment,'EmpJobRole' : EmpJobRole,
